67c51dd201c6190986764dccf522a77ac4789d09
sandeep <sandeep.chavan@accesscfa.com>
2022-04-30 19:27:36 -0400

    test

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          JSR
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sandbox.d2xbbflxlbwc74.amplifyapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Batch Visibility POC
         </a>
       </header>
     </div>
