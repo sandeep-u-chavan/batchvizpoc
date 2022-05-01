016d39ed8512910df5585af2fa456d2ae7c6ed53
sandeep <sandeep.chavan@accesscfa.com>
2022-04-30 23:03:32 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator} from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          JSR
-        </p>
-        <a
-          className="App-link"
-          href="https://sandbox.d2xbbflxlbwc74.amplifyapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Batch Visibility POC
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>JSR Batch Visibility POC</h1>
+            <button onClick={signOut}>Sign out</button>
+        </header>
+      </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
